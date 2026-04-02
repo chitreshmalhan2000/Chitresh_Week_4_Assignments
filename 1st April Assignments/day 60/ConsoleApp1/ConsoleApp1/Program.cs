331a504f253119832250594fd1423bf5841f282a
chitreshmalhan2000 <161840338+chitreshmalhan2000@users.noreply.github.com>
2026-04-02 14:35:51 +0530

    1st april assignments

---
+++ 1st April Assignments/day 60/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using Azure.Storage.Blobs;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static string str_connection = "DefaultEndpointsProtocol=https;AccountName=chitreshstorage;AccountKey=b4bBoTN8jLDnZjW3qo3UHDjC/Uo6IbyBOX8jLcc/jDhhjIbayRZTIBYl3WSDi+IKX7eYc5EEMh0s+AStUsAqBA==;EndpointSuffix=core.windows.net";// this value i had kept empty u have to take it from azure access keys and add it and in github u cant put sececet 
+        // connection like this becasue when u try to commit it tells secret information is there so for security reasong i cannot do commit so removed
+
+        static async Task Main()
+        {
+            BlobServiceClient blobServiceClient = new BlobServiceClient(str_connection);
+            BlobContainerClient container_client =
+            blobServiceClient.GetBlobContainerClient("data");
+            BlobClient blob_client = container_client.GetBlobClient("file.txt");
+            using FileStream uploadFileStream = File.OpenRead(@"D:\newpass.txt");
+            await blob_client.UploadAsync(uploadFileStream, true);
+            uploadFileStream.Close();
+            Console.WriteLine("File uploaded");
+            Console.WriteLine("Operation complete");
+        }
+    }
+}
---
+++ 1st April Assignments/day 60/WebAPIASPDOTNETDEMO/Migrations/20260401083544_initialcreate.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WebAPIASPDOTNETDEMO.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialcreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "171193fb-cb5f-4ba3-975e-fb8d472ec95a");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3d057026-7f74-4dde-b77a-8dc03aa10a54");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b200f684-3da8-4496-adbf-6960f0ea80c6");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "1f10be1b-b033-4280-bd2e-2f166a36c744", "2", "User", "User" },
+                    { "46cf7514-48ce-4819-b7e2-00ba3ad477f8", "1", "Admin", "Admin" },
+                    { "f339a7fe-70b8-4c0f-b342-d209156a2703", "3", "HR", "HR" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1f10be1b-b033-4280-bd2e-2f166a36c744");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "46cf7514-48ce-4819-b7e2-00ba3ad477f8");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "f339a7fe-70b8-4c0f-b342-d209156a2703");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "171193fb-cb5f-4ba3-975e-fb8d472ec95a", "2", "User", "User" },
+                    { "3d057026-7f74-4dde-b77a-8dc03aa10a54", "1", "Admin", "Admin" },
+                    { "b200f684-3da8-4496-adbf-6960f0ea80c6", "3", "HR", "HR" }
+                });
+        }
+    }
+}
